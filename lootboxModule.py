4f02ac56652375762c1075f64094b84cdbfe9272
German Migdalovich <germanmigd@gmail.com>
2026-03-27 22:50:24 +0000

    Starting feature work

--- lootboxModule.py
+++ lootboxModule.py
@@ -5,34 +5,50 @@ gameLoot = {
     "rare" : ["machete","pistol","baseball bat"],
     "epic" : ["bolt action","shotgun","chainsaw"],
     "legendary" : ["50 Cal","silenced pistol","Shadow Kunai"]
-    
 }
 
+lootboxTypes = {
+    "common" : [60,85,97],
+    "rare" : [50,75,96],
+    "epic": [40,55,95],
+    "legendary": [30,50,90]
+}
 
 def rarityGenerator():
     x = random.randint(1,100)
     return x
     
-def lootGenerator():
+def lootGenerator(lootbox):
+
     lootnum = rarityGenerator()
 
-    if lootnum < 60:
-        return "common"
+    print(f'This is lootnum index {lootbox["common"[1]]}')
 
-    elif lootnum < 85:
-        return "rare"
-    
-    elif lootnum < 97:
-        return "epic"
-    
-    else:
-        return "legendary"
+    # if lootnum < lootbox[1]:
+    #     return "common"
+
+    # elif lootnum < lootbox[2]:
+    #     return "rare"
+
+    # elif lootnum < lootbox[3]:
+    #     return "epic"
+
+    # else:
+    #     return "legendary"
     
 
 def lootbox():
+
     rarity = lootGenerator()
     item = random.choice(gameLoot[rarity])
-    return f'{rarity.upper()}: {item}'
 
-for i in range(1000):
-    print(lootbox())
+    loot = {
+        "rarity" : rarity,
+        "item" : item
+    }
+
+    # return f'{rarity.upper()}: {item}'
+    return loot
+
+
+print(lootGenerator("common"))
--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from lootboxModule import lootbox
 
 loot = lootbox()
 
+itemRarity = {
+    "common" : "black",
+    "epic" : "blue",
+    "rare" : "purple",
+    "legendary" : "red"
+}
+
+print(loot)
+
 window = tk.Tk()
 
-label = tk.Label(window, text=f"{loot}")
+label = tk.Label(window, text=f"{loot["item"]}", fg = itemRarity[loot["rarity"]])
 
 window.geometry("500x500")
 
