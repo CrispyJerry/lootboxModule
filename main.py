0c0440a8fb2471e73da7be1003f6cd6769b6649b
G <germanmigd@gmail.com>
2026-03-28 15:20:46 +0000

    added tkinter button with function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import tkinter as tk
-from lootboxModule import lootbox
+from lootboxModule import *
+import time
 
 loot = lootbox("common")
 
+def getLootbox():
+    loot = lootbox("common")
+    return loot
+    
 itemRarity = {
     "common" : "black",
     "epic" : "blue",
@@ -15,9 +20,16 @@ print(loot)
 window = tk.Tk()
 
 label = tk.Label(window, text=f"{loot["item"]}", fg = itemRarity[loot["rarity"]])
+label.pack()
 
+def rollLoot():
+    loot = getLootbox()
+    label.config(text = f"{loot["item"]}",fg = itemRarity[loot["rarity"]])
+
+
+button = tk.Button(window,text="Open Lootbox",command= rollLoot)
+button.pack()
 window.geometry("200x200")
 
-label.pack()
 
 window.mainloop()
